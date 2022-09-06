6be71e56bff0103ff207117e883f3fa7706dca0a
Breiner Jimenez <breiner1803@gmail.com>
2022-09-06 11:03:30 -0500

    Pong v1.1: Scores

--- setup.py
+++ setup.py
@@ -3,39 +3,54 @@ import pygame, sys, random
 
 def ball_restart():
     global ball_speed_x, ball_speed_y
+
     ball.center = (screen_width/2,screen_height/2)
     ball_speed_y *= random.choice((1,-1))
     ball_speed_x *= random.choice((1,-1))
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_score, opponent_score
+
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
+
+    if ball.left <= 0:
         ball_restart()
+        player_score += 1
+
+    if ball.right >= screen_width:
+        ball_restart()
+        opponent_score += 1
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
 
 
 def player_animation():
+
     player.y += player_speed
+
     if player.top <= 0:
         player.top = 0
+
     if player.bottom >= screen_height:
         player.bottom = screen_height      
 
 
 def opponent_ai():
+
     if opponent.top < ball.y:
         opponent.top += opponent_speed
+
     if opponent.bottom > ball.y:
         opponent.bottom -= opponent_speed
+
     if opponent.top <= 0:
         opponent.top = 0
+
     if opponent.bottom >= screen_height:
         opponent.bottom = screen_height       
 
@@ -56,13 +71,20 @@ ball = pygame.Rect(screen_width/2 - 15,screen_height/2 - 15,30,30)
 player = pygame.Rect(screen_width - 20,screen_height/2 - 70,10,140)
 opponent = pygame.Rect(10,screen_height/2 - 70,10,140)
 
+#Colors
 bg_color = pygame.Color('grey12')
 light_grey = (200,200,200)
 
+#Game Variables
 ball_speed_x = 7 * random.choice((1,-1))
 ball_speed_y = 7 * random.choice((1,-1))
 player_speed = 0
 opponent_speed = 7
+
+#Text Variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf",32)
 
 while True:
     # Handling input
@@ -72,15 +94,16 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                player_speed += 7
+                player_speed += 6
             if event.key == pygame.K_UP:
-                player_speed -= 7
+                player_speed -= 6
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
-                player_speed -= 7
+                player_speed -= 6
             if event.key == pygame.K_UP:
-                player_speed += 7
-        
+                player_speed += 6
+
+    # Game Logic    
     ball_animation()
     player_animation()
     opponent_ai()
@@ -90,7 +113,13 @@ while True:
     pygame.draw.rect(screen,light_grey,player)
     pygame.draw.rect(screen,light_grey,opponent)
     pygame.draw.ellipse(screen,light_grey,ball)
-    pygame.draw.aaline(screen,light_grey,(screen_width/2,0),(screen_width/2,screen_height))        
+    pygame.draw.aaline(screen,light_grey,(screen_width/2,0),(screen_width/2,screen_height))     
+
+    player_text = game_font.render(f"{player_score}",False,light_grey)
+    screen.blit(player_text,(380,240))
+
+    opponent_text = game_font.render(f"{opponent_score}",False,light_grey)
+    screen.blit(opponent_text,(320,240))
 
     # Updating the window
     pygame.display.flip()
